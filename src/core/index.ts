5fcc9fc5b92cb586cd011e1924bfed81b34612bd
Moritz <mgottsc5@smail.uni-koeln.de>
2022-05-31 18:05:07 +0200

    Remove old code

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,10 +3,8 @@ import {
   getCoreRowModel,
   PaginationState,
   ReactTableGenerics,
-  Render,
   Table as ReactTable,
   TableInstance as ReactTableInstance,
-  TableState as ReactTableState,
   useTableInstance,
   UseTableInstanceOptions,
 } from "@tanstack/react-table";
@@ -59,8 +57,6 @@ export function useTableus<T extends ReactTableGenerics>(
     reactTableOptions: reactTableOptionsProp,
   } = options;
 
-  // const [laggedTableState, setLaggedTableState] = useState<ReactTableState>();
-
   const context = useContext(TableusContext);
   const tableusConfig = context?.config;
   if (!tableusConfig) {
@@ -90,18 +86,6 @@ export function useTableus<T extends ReactTableGenerics>(
     }
   }, [paginationState]);
 
-  // const [plugins, pluginConfigs] = useMemo((): [
-  //   PluginHook<D>[],
-  //   Record<any, any>[]
-  // ] => {
-  //   if (tableConfig === undefined) return [[], []];
-  //   const pluginConfigs = getPlugins<D>(tableConfig, { paginationState });
-  //   const plugins = pluginConfigs.map((c) => c.plugin);
-  //   const configs = pluginConfigs.map((c) => c.config);
-  //   return [plugins, configs];
-  // }, [tableConfig, paginationState]);
-  //
-  //
   const reactTableOptions: UseTableInstanceOptions<T> = useMemo(() => {
     const configs: Partial<UseTableInstanceOptions<T>>[] = [
       {
@@ -114,35 +98,13 @@ export function useTableus<T extends ReactTableGenerics>(
         // consider autoResetAll as this already caused a hard to debug bug
         autoResetPageIndex: false,
       },
-      // ...pluginConfigs,
     ];
     if (reactTableOptionsProp !== undefined)
       configs.push(reactTableOptionsProp);
     return deepmerge.all<UseTableInstanceOptions<T>>(configs);
-    // }, [data, reactTableColumns, pluginConfigs,
-    // options.reactTableOptions]);
-    // }, [data, reactTableColumns, pluginConfigs, reactTableOptionsProp]);
   }, [data, reactTableColumns, reactTableOptionsProp]);
 
-  const reactTableInstance = useTableInstance(
-    table,
-    reactTableOptions
-    // ...plugins
-  );
-  // const [tableState, setTableState] = useState<ReactTableState>(
-  //   reactTableInstance.initialState
-  // );
-  // useEffect(() => {
-  //   setLaggedTableState(tableState);
-  // }, [tableState]);
-
-  // const setPagination = useCallback(
-
-  // reactTableInstance.setOptions((prev) => ({
-  //   ...prev,
-  //   state: tableState,
-  //   onStateChange: setTableState,
-  // }));
+  const reactTableInstance = useTableInstance(table, reactTableOptions);
 
   return {
     tableusProps: {
@@ -155,38 +117,3 @@ export function useTableus<T extends ReactTableGenerics>(
     reactTableInstance: reactTableInstance,
   };
 }
-
-// interface PluginConfig<D extends object> {
-//   plugin: PluginHook<D>;
-//   config: object;
-// }
-
-// function getPlugins<D extends object>(
-//   config: TableConfig,
-//   dynamicPluginConfig: { paginationState?: PaginationMeta }
-// ) {
-//   const pluginConfigs: PluginConfig<D>[] = [];
-//
-//   if (config.pagination)
-//     pluginConfigs.push(
-//       getPagination<D>(config, dynamicPluginConfig.paginationState)
-//     );
-//
-//   return pluginConfigs;
-// }
-//
-// function getPagination<D extends object>(
-//   config: TableConfig,
-//   paginationState?: PaginationMeta
-// ): PluginConfig<D> {
-//   return {
-//     plugin: usePagination,
-//     config: {
-//       manualPagination: true,
-//       pageCount: paginationState?.pageCount ?? 10,
-//       initialState: {
-//         pageSize: paginationState?.pageIndex ?? config.pageSize,
-//       },
-//     },
-//   };
-// }
